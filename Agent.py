08d6c94622e015ea749bf0180fcc3e47c70407ac
Yash Shah <74123050+yshah9103@users.noreply.github.com>
2021-11-05 09:24:32 -0400

    Revert "Path Planner Addition"

--- Agent.py
+++ Agent.py
@@ -7,15 +7,13 @@ UNACCESSIBLE_ROOM_VALUE = 0
 AGENT_LOCATION_VALUE = 0.25
 
 class Agent:
-  def __init__(self, starting_location, target_location) -> None:
+  def __init__(self, starting_location) -> None:
     self.current_location = np.array(starting_location) # Current Location in the grid
     self.distance_from_meeting_point = 0                # How long will it take to get to meeting point
     self.is_searching = True                            # New location when search True, 
                                                             # Plan to meeting point when False
-    self.movement_history = []
-    self.final_location=np.array(target_location)                          # History of locations that agent has been to
-    self.next_location = np.array(starting_location)
-    self.path=[]    # Next Location planned for the agent
+    self.movement_history = []                          # History of locations that agent has been to
+    self.next_location = np.array(starting_location)    # Next Location planned for the agent
 
 ### @Function: Updates the Agent Location from next to current
 #
--- StateMachine.py
+++ StateMachine.py
@@ -3,23 +3,21 @@ from Agent import *
 from PathPlanner import *
 
 class StateMachine:
-  def __init__(self, raw_floor_plan, number_of_agents, agent_entry_location, meeting_location, agent_exit_location, target_location, init_searching_timespan):
+  def __init__(self, raw_floor_plan, number_of_agents, agent_entry_location, meeting_location, init_searching_timespan):
     self.raw_floor_plan = raw_floor_plan
     self.scanned_floor_plan = deepcopy(raw_floor_plan)
     self.list_of_agents = []
     self.meeting_location = meeting_location
-    self.target_location = target_location
     self.searching_timespan = init_searching_timespan
 
-    self.initClassObjects(number_of_agents, agent_entry_location, agent_exit_location)
+    self.initClassObjects(number_of_agents, agent_entry_location)
 
   ### Function: Initialize Agent and Planner class objects
   #
   #
-  def initClassObjects(self, number_of_agents, agent_entry_location,agent_exit_location):
+  def initClassObjects(self, number_of_agents, agent_entry_location):
     for i in range(number_of_agents):
-      path=[]
-      self.list_of_agents.append(Agent(agent_entry_location,agent_exit_location[i]))
+      self.list_of_agents.append(Agent(agent_entry_location))
 
     # Init Path Planner Object
     self.path_planner = PathPlanner(self.scanned_floor_plan, self.meeting_location, self.list_of_agents)
@@ -28,15 +26,11 @@ class StateMachine:
   ### Function: Plans the next state and returns the map to be animated
   #
   #
-
-  def getNextState(self,iterate_path_count):
-    if(iterate_path_count==0):
-      self.path_planner.getNextMoves()
+  def getNextState(self):
     # Plan next steps for all the agents
+    self.path_planner.planNextSteps()
 
-    self.path_planner.planNextSteps(iterate_path_count)
-    iterate_path_count+=1
-
+      
     # This copy will be needed to display the agents' locations
     copy_of_scanned_plan = deepcopy(self.scanned_floor_plan)
 
